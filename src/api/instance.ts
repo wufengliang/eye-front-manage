a6daa6e1d016283b182c94abba6d517fb83b3406
wufengliang <44823912@qq.com>
2023-11-03 16:56:50 +0800

    fix: 🐛 业务code兼容

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: wufengliang 44823912@qq.com
  * @Date: 2023-08-31 14:50:25
- * @LastEditTime: 2023-11-02 15:53:03
+ * @LastEditTime: 2023-11-03 16:53:06
  * @Description: 拦截器
  */
 import axios from 'axios';
@@ -35,7 +35,7 @@ instance.interceptors.response.use(response => {
   const { code, message, data: result } = response.data;
   switch (response.status) {
     case 200:
-      if ([403, 404, 500, 4006].includes(code)) {
+      if ([403, 404, 500, 4006, 4000].includes(code)) {
         notification.error({ message: '提示', description: message || '接口异常', duration: 2 });
         return Promise.reject(response);
       }
